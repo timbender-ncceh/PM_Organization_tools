dd97357edff603537904bd74335130227f7bf0c0
timbender-ncceh <105810134+timbender-ncceh@users.noreply.github.com>
2023-02-27 20:57:51 -0500

    Create secret_ical_url.R

---
+++ secret_ical_url.R
@@ -0,0 +1,7 @@
+# secret_ical_url.R
+
+# set google calendar ical urls (secret) for PM calendar and MTG calendar below.  
+# stand-alone so can be gitignored
+
+secret.ical.url_PM <- "<secret_ical_url>"
+secret.ical.url_MTG <- "<secret_ical_url>"
